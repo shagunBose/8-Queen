9fc8e785b5b583922908e675dd88852a4348655f
Shagun Bose <32908915+shagunBose@users.noreply.github.com>
2018-03-17 16:35:10 -0400

    create variable to track instantaneous position of queen

--- Queen.java
+++ Queen.java
@@ -3,9 +3,12 @@ public class Queen {
 	
 	int placedAtRow;
 	int placedAtColumn;
+	int X_Position;
+	int Y_Position;
 	boolean inFocus;
 	boolean isDragging;
 	boolean placed;
 	boolean safe;
+	
 
 }
