077c9a02d150bce0fd614830ddc422cb7eaa25d2
Shagun Bose <32908915+shagunBose@users.noreply.github.com>
2018-03-17 16:54:57 -0400

    create constructor in Queen class
    takes variable x, y to define the X and Y position of queen

--- Queen.java
+++ Queen.java
@@ -4,13 +4,18 @@ public class Queen {
 	
 	int placedAtRow;
 	int placedAtColumn;
-	int X_Position;
+	int X_Position; 
 	int Y_Position;
 	boolean inFocus;
 	boolean isDragging;
 	boolean placed;
 	boolean safe;
+	
 	Color color; //unsafe queen is red, safe queen is blue
 	
-
+	//constructor
+	public Queen(int x, int y) {
+		X_Position = x;
+		Y_Position = y;
+	}
 }
