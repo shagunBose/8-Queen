019d0c059113d3d35d109d0261b5a48d0c65d124
Shagun Bose <32908915+shagunBose@users.noreply.github.com>
2018-03-17 16:57:13 -0400

    implement MouseListeners to Queen class

--- Queen.java
+++ Queen.java
@@ -1,6 +1,9 @@
 import java.awt.Color;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
-public class Queen {
+public class Queen implements MouseListener, MouseMotionListener{
 	
 	int placedAtRow;
 	int placedAtColumn;
@@ -17,5 +20,44 @@ public class Queen {
 	public Queen(int x, int y) {
 		X_Position = x;
 		Y_Position = y;
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub	
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
